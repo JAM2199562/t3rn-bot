4a6e4d934e86c16e58b27beba6f60760167dfcc7
JAM2199562 <JAM2199562@maricopa.edu>
2025-01-13 20:39:21 +0800

    更换全部 rpc

--- network_config.py
+++ network_config.py
@@ -2,22 +2,22 @@
 
 networks = {
     'Arbitrum Sepolia': {
-        'rpc_url': 'https://sepolia-rollup.arbitrum.io/rpc',
+        'rpc_url': 'https://rpc.ankr.com/arbitrum_sepolia/c0ca98c5d377c61f60b34732aab8d28f0583810b5817fd9f86efc9d540228028',
         'chain_id': 421614,
         'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D'
     },
     'Blast Sepolia': {
-        'rpc_url': 'https://sepolia.blast.io',
+        'rpc_url': 'https://rpc.ankr.com/blast_testnet_sepolia/c0ca98c5d377c61f60b34732aab8d28f0583810b5817fd9f86efc9d540228028',
         'chain_id': 168587773,
         'contract_address': '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2'
     },
     'Optimism Sepolia': {
-        'rpc_url': 'https://sepolia.optimism.io',
+        'rpc_url': 'https://rpc.ankr.com/optimism_sepolia/c0ca98c5d377c61f60b34732aab8d28f0583810b5817fd9f86efc9d540228028',
         'chain_id': 11155420,
         'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'  # 需要替换为实际地址
     },
     'Base Sepolia': {
-        'rpc_url': 'https://sepolia.base.org',
+        'rpc_url': 'https://rpc.ankr.com/base_sepolia/c0ca98c5d377c61f60b34732aab8d28f0583810b5817fd9f86efc9d540228028',
         'chain_id': 84532,
         'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5'  # 需要替换为实际地址
     }
